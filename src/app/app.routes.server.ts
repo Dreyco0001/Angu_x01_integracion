62a021dcad0ffc6872ffd7a5abc4cebbf3abc058
Dreyco001 <dreycoarak00@gmail.com>
2025-05-16 15:04:53 -0400

    agregando

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { LoginComponent } from './login/login.component';
 
 export const serverRoutes: ServerRoute[] = [
+  {path: 'login',
+    component: LoginComponent,
+    data: {
+      // Aquí puedes controlar el renderizado
+      // Esto sería útil si el login no necesita SEO
+      renderMode: 'browser'  // Evita renderizado SSR},
+      }
+    },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+      // Aquí haces el request al backend
+    } else {
+      console.error('Formulario inválido');
+    }
+  }
+}
