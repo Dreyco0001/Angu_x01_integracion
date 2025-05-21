975de7521c2a8cc6a0045b74306a1ef80cb25269
Dreyco001 <dreycoarak00@gmail.com>
2025-05-21 16:36:48 -0400

    primera version del menu

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,13 +1,36 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'login',
     renderMode: RenderMode.Client
   },
   {
+    path: 'registro',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'gestion',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'carrito',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'perfil-usuario',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'tienda',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
+import { TiendaComponent } from './tienda/tienda.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
+import { AdminComponent } from './admin/admin.component';
+import { GestionComponent } from './gestion/gestion.component';
+import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: '**', redirectTo: 'login'}
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'perfil-usuario', component: PerfilUsuarioComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'gestion', component: GestionComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'tienda', pathMatch: 'full' },  // Tienda es el nuevo home
+  { path: '**', redirectTo: 'tienda' }  // Catch-all
 ];
